6400690bc951bb53963514883908e4ec9d71ca51
MMD <manuelmoralesdiaz0@gmail.com>
2024-04-10 10:01:56 -0400

    deleting files and commiting first task

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,17 +1,18 @@
-#!/usr/bin/python3
-""" This module starts a Flask web application """""
+#!/usr/bin/pyhton3
+""" Script to start a Flask web app """
+
 from flask import Flask
-"""Flask class and render_template method"""
+
+""" Creacion de instancia de la aplicación Flask """ 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
-""" App route section"""
-
-
-@app.route('/')
-def hello():
+""" Definir la ruta principal ("/") con el mensaje "Hello HBNB!" """
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
     return 'Hello HBNB!'
 
-
 if __name__ == '__main__':
+    """
+    Configurar la aplicación para que escuche en 0.0.0.0 en el puerto 5000
+    """
     app.run(host='0.0.0.0', port=5000)
