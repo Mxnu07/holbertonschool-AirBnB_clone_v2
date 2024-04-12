88524d156f6e5a1e90929c262563c239dc3abbe5
MMD <manuelmoralesdiaz0@gmail.com>
2024-04-11 21:29:31 -0400

    switching def order

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -7,15 +7,15 @@ from models.state import State
 app = Flask(__name__)
 
 
+@app.teardown_appcontext
+def close_session(self):
+    storage.close()
+
+
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     states = storage.all(State)
     return render_template('7-states_list.html', states=states)
-
-
-@app.teardown_appcontext
-def close_session(self):
-    storage.close()
 
 
 if __name__ == '__main__':
