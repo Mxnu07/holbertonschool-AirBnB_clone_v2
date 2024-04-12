3de1d1593167fecc004403ce777baedd8d3989fd
MMD <manuelmoralesdiaz0@gmail.com>
2024-04-12 18:30:20 -0400

    debugging

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -11,8 +11,7 @@ app = Flask(__name__)
 @app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
     states = storage.all(State)
-    sorted_states = sorted(states.values(), key=lambda state: state.name)
-    return render_template('8-cities_by_states.html', states=sorted_states)
+    return render_template('8-cities_by_states.html', states=states)
 
 
 @app.teardown_appcontext
