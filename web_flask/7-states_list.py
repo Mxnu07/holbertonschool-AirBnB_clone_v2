b0b48c0f79d46b479d53f894840b7ab802d2a27b
MMD <manuelmoralesdiaz0@gmail.com>
2024-04-11 22:26:24 -0400

    debugging

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -15,7 +15,7 @@ def teardown(self):
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-    states = storage.all(State)
+    states = storage.all("State")
     return render_template('7-states_list.html', states=states)
 
 
