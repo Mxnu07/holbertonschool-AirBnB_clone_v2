c30ac2ea647a42ec8fcd857ef5a4b6b9d6ea380d
MMD <manuelmoralesdiaz0@gmail.com>
2024-04-11 21:17:53 -0400

    debugging

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -10,13 +10,11 @@ app = Flask(__name__)
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     states = storage.all(State)
-    sorted_states = sorted(states.values(), key=lambda state: state.name)
-
-    return render_template('7-states_list.html', states=sorted_states)
+    return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
-def close_session(exception=None):
+def close_session(self):
     storage.close()
 
 
